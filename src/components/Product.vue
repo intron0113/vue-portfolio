ee2a8021f8875f97fc4cb400e8a9ab83e43ebfe2
intron0113 <83868900+intron0113@users.noreply.github.com>
2021-09-14 12:28:32 +0900

    master

---
+++ src/components/Product.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-col cols="12" sm="6" md="4" lg="4">
+    <v-card class="mx-auto">
+      <v-img
+        class="white--text align-end"
+        src="https://dummyimage.com/250x150/000/fff"
+      >
+        <v-card-title></v-card-title>
+      </v-img>
+      <v-card-subtitle class="pb-0"></v-card-subtitle>
+
+      <v-card-text class="text--primary">
+        <div>{{ product.category }}</div>
+        <div>{{ product.use }}</div>
+        <div>{{ product.info }}</div>
+      </v-card-text>
+
+      <v-card-actions>
+        <v-btn clor="orange" text>
+          LINK
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-col>
+</template>
+
+<script>
+export default {
+  name: 'Product',
+  props: ['product'],
+};
+</script>
+<style></style>
---
+++ src/components/ProductList.vue
@@ -0,0 +1,91 @@
+<template>
+  <div>
+    <ProductHeader @sortYearChange="sortYearChanged($event)"></ProductHeader>
+    <v-row>
+      <product
+        v-for="(product, key) in filterdList"
+        :key="key"
+        :product="product"
+      >
+      </product
+    ></v-row>
+  </div>
+</template>
+
+<script>
+import Product from './Product';
+import ProductHeader from './ProductHeader';
+
+export default {
+  name: 'ProductList',
+  components: { Product, ProductHeader },
+  data: () => ({
+    sortYear: '全て',
+    products: [
+      {
+        id: 1,
+        name: 'work1',
+        image: '../assets/works/dummyimage.png',
+        year: '2020',
+        category: 'コーポレート',
+        use: 'html css JS',
+      },
+      {
+        id: 2,
+        name: 'work2',
+        image: '../assets/works/dummyimage.png',
+        year: '2019',
+        category: 'LP',
+        use: 'Vue.js',
+        info: '',
+      },
+      {
+        id: 3,
+        name: 'work3',
+        image: '../assets/works/dummyimage.png',
+        year: '2018',
+        category: 'LP',
+        use: 'デザイン',
+      },
+      {
+        id: 4,
+        name: 'work4',
+        image: '../assets/works/dummyimage.png',
+        year: '2019',
+        category: 'LP',
+        use: 'Vue.js',
+        info: '',
+      },
+    ],
+  }),
+  methods: {
+    sortYearChanged: function(year) {
+      console.log(year);
+      this.sortYear = year;
+    },
+  },
+  computed: {
+    filterdList: function() {
+      const newList = [];
+      for (let i = 0; i < this.products.length; i++) {
+        if (this.sortYear === '2020' && this.products[i].year === '2020') {
+          newList.push(this.products[i]);
+        } else if (
+          this.sortYear === '2019' &&
+          this.products[i].year === '2019'
+        ) {
+          newList.push(this.products[i]);
+        } else if (
+          this.sortYear === '2018' &&
+          this.products[i].year === '2018'
+        ) {
+          newList.push(this.products[i]);
+        } else if (this.sortYear === '全て') {
+          newList.push(this.products[i]);
+        }
+      }
+      return newList;
+    },
+  },
+};
+</script>
